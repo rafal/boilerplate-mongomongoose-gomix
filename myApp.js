28a3f5353c7a4385d9d595da963bf81da7df73e2
Rafał Płaszczyk <rafalplaszczyk.com@gmail.com>
2021-03-18 23:32:16 +0000

    Chain Search Query Helpers to Narrow Search Results

--- myApp.js
+++ myApp.js
@@ -104,8 +104,11 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}
+  ).sort({name: 1}).limit(2).select({age: 0}).exec((error, data) => {
+    if (error) return console.error(error)
+    console.log(data)
+    done(null, data)})
 };
 
 /** **Well Done !!**
